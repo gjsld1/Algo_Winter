dd3be424e0db6c9c09be6c0f81c30d15706155ee
신예지 <gjsld1@naver.com>
2019-01-23 15:06:21 +0900

    2557

---
+++ Algo_Winter/2557.cpp
@@ -0,0 +1,14 @@
+//
+//  2557.cpp
+//  Algo_Winter
+//
+//  Created by 신예지 on 23/01/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello World!";
+}
