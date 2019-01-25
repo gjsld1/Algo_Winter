1851b7c3e9aa5674254e9fb2391807a5ca352fb0
신예지 <gjsld1@naver.com>
2019-01-25 17:44:13 +0900

    10718 11718(공백 문제)

---
+++ Algo_Winter/10718.cpp
@@ -0,0 +1,16 @@
+//
+//  10718.cpp
+//  Algo_Winter
+//
+//  Created by 신예지 on 25/01/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str="강한친구 대한육군";
+    cout<<str<<endl<<str;
+}
---
+++ Algo_Winter/11718.cpp
@@ -0,0 +1,17 @@
+//
+//  11718.cpp
+//  Algo_Winter
+//
+//  Created by 신예지 on 25/01/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str, str2, str3;
+    cin>>str>>str2>>str3;
+    cout<<str<<endl<<str2<<endl<<str3;
+}
